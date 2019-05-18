94e74993d39ee1ce9406ff5a57d789e99832e182
umarovkamalov <umarovkamalov@gmail.com>
2019-05-18 09:14:46 +0500

    controller

---
+++ controller.php
@@ -0,0 +1,16 @@
+$token = "628414083:AAFY6cmu2LhLS32QifYcjh1HlowftkdUVUc";
+$bot = new \TelegramBot\Api\Client($token);
+// команда для start
+$bot->command('start', function ($message) use ($bot) {
+    $answer = '„обро пожаловать!';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+// команда для помощи
+$bot->command('help', function ($message) use ($bot) {
+    $answer = 'Команды:
+/help - вывод справки';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+$bot->run();
